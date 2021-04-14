ec016f17a094b9e5508138877ec5a13747600d8b
Anna Klara Lagerås <annaklara@Annas-MacBook-Pro.local>
2021-04-14 14:55:56 -0500

    test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='''A project layout for reproducible work''',
+    author='Niven Shumaker',
+    license='BSD 3-Clause',
+)
---
+++ tasks.py
@@ -0,0 +1,55 @@
+from os import environ
+from sys import platform
+
+from invoke import task
+
+
+# Set
+ENV_NAME = 'seis_feature_env'
+
+# Set the runing shell to PowerShell
+# https://github.com/pyinvoke/invoke/pull/407
+# TODO Test if this really works on other than Windows platforms
+if True:
+    #SHELL = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'
+    SHELL = '/bin/bash'
+else:
+    # default was cmd.exe
+    if platform == 'win32':
+        SHELL = environ['COMSPEC']
+    else:
+        SHELL  = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'
+
+
+# Run this first
+# conda env create --name my_env_name --file .\environment.yml
+# conda activate
+
+
+@task
+def env_set_jupyter(c):
+    print('Setting up jupyter kernel')
+    c.run(
+        f"ipython kernel install --name {ENV_NAME} --display-name {ENV_NAME} --sys-prefix")
+    print('Adding nbextensions')
+    c.run("jupyter nbextensions_configurator enable --user")
+    print('Done!')
+
+
+@task
+def env_to_freeze(c):
+    c.run(f"conda env export --name {ENV_NAME} --file environment_to_freeze.yml",
+          shell=SHELL)
+    print('Exported freeze environment to: environment_to_freeze.yml')
+
+
+@task
+def env_update(c):
+    c.run(f"conda env update --name {ENV_NAME} --file environment.yml --prune",
+          shell=SHELL)
+
+
+@task
+def env_remove(c):
+    c.run(f"conda remove --name {ENV_NAME} --all",
+          shell=SHELL)
